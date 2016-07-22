9f91b15770f36c5d27093689e5f0e8ab3dac3c41
Luiz Carlos <luiz.manhani@gmail.com>
2016-07-22 15:00:37 -0300

    csslint e jshint

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,10 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     usemin = require('gulp-usemin'),
     cssmin = require('gulp-cssmin'),
-    browserSync = require('browser-sync');
+    browserSync = require('browser-sync'),
+    jshint = require('gulp-jshint'),
+    jshintStylish = require("jshint-stylish"),
+    csslint = require("gulp-csslint");
 
 gulp.task('default',['copy'],function() {
     gulp.start(['build-img','usemin']);
@@ -43,5 +46,15 @@ gulp.task('server',function(){
          baseDir : 'src'
      }
    });
+    gulp.watch('src/js/*.js').on('change',function(event){
+        gulp.src(event.path)
+            .pipe(jshint())
+            .pipe(jshint.reporter(jshintStylish));
+    });
+    gulp.watch('src/css/*.css').on('change',function(event){
+        gulp.src(event.path)
+            .pipe(csslint())
+            .pipe(csslint.reporter());
+    });
     gulp.watch('src/**/*').on('change',browserSync.reload);
 });
