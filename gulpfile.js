09a17f3f7a0ad0b02c6bb5ba544a50418f97ec20
Luiz Carlos <luiz.manhani@gmail.com>
2016-07-22 12:47:54 -0300

    Unindo todos os arquivos js em um all.js e minificando

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 var gulp = require('gulp'),
     imagemin = require('gulp-imagemin'),
-    clean = require('gulp-clean');
+    clean = require('gulp-clean'),
+    concat = require('gulp-concat'),
+    htmlReplace = require('gulp-html-replace'),
+    uglify = require('gulp-uglify');
+
+gulp.task('default',['copy'],function() {
+    gulp.start(['build-img','build-js','build-html']);
+});
 
 gulp.task('copy',['clean'],function(){
     return gulp.src('src/**/*')
@@ -12,8 +19,23 @@ gulp.task('clean',function(){
         .pipe(clean());
 });
 
-gulp.task('build-img',['copy'],function () {
+gulp.task('build-img',function () {
     gulp.src('dist/img/**/*')
         .pipe(imagemin())
         .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('build-js',function(){
+    gulp.src(['dist/js/jquery.js','dist/js/home.js','dist/js/produto.js'])
+        .pipe(concat('all.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'))
+});
+
+gulp.task('build-html',function(){
+    gulp.src('dist/**/*.html')
+        .pipe(htmlReplace({
+            js:'js/all.js'
+        }))
+        .pipe(gulp.dest('dist'));
 });
