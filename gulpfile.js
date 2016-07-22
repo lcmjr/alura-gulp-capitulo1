bd1c664eb1f6c8115755edb2f976453c81588055
Luiz Carlos <luiz.manhani@gmail.com>
2016-07-22 12:56:58 -0300

    retirando o build-js e build-html, utilizando plugin usemin

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,12 @@ var gulp = require('gulp'),
     clean = require('gulp-clean'),
     concat = require('gulp-concat'),
     htmlReplace = require('gulp-html-replace'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    cssmin = require('gulp-cssmin');
 
 gulp.task('default',['copy'],function() {
-    gulp.start(['build-img','build-js','build-html']);
+    gulp.start(['build-img','usemin']);
 });
 
 gulp.task('copy',['clean'],function(){
@@ -25,17 +27,11 @@ gulp.task('build-img',function () {
         .pipe(gulp.dest('dist/img'));
 });
 
-gulp.task('build-js',function(){
-    gulp.src(['dist/js/jquery.js','dist/js/home.js','dist/js/produto.js'])
-        .pipe(concat('all.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js'))
-});
-
-gulp.task('build-html',function(){
-    gulp.src('dist/**/*.html')
-        .pipe(htmlReplace({
-            js:'js/all.js'
-        }))
-        .pipe(gulp.dest('dist'));
+gulp.task('usemin',function(){
+   gulp.src('dist/**/*.html')
+       .pipe(usemin({
+           'js' : [uglify],
+           'css' : [cssmin]
+       }))
+       .pipe(gulp.dest('dist'));
 });
